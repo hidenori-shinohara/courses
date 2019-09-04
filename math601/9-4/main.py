c45a79dee160d0b4ba3b799b234a5516353df677
Hide Shinohara <hidenori.shinohara.jp@gmail.com>
2019-09-04 07:31:55 -0400

    Finish Problem 2-6(ii) 601

--- math601/9-4/main.py
+++ math601/9-4/main.py
@@ -1,20 +1,35 @@
 
+from sets import Set
 
-ps = [2, 3, 5, 7, 11, 13, 17, 19]
+ps = [5]
+
+# for p in ps: 
+#     cnt = 0
+#     shouldPrint = False
+#     for a in range(p):
+#         for b in range(p):
+#             for c in range(p):
+#                 for d in range(p):
+#                     if not (a * d - b * c) % p == 0:
+#                         cnt = cnt + 1
+#                         if shouldPrint and cnt <= 10:
+#                             print a, b
+#                             print c, d
+#                             print
+#                         
+#     print("prime = %d, cnt = %d" % (p, cnt))
+#     print("my formula = %d" % (p**4 - p**3 - p**2 + p))
 
 for p in ps: 
-    cnt = 0
-    shouldPrint = False
-    for a in range(p):
-        for b in range(p):
-            for c in range(p):
-                for d in range(p):
-                    if not (a * d - b * c) % p == 0:
-                        cnt = cnt + 1
-                        if shouldPrint and cnt <= 10:
-                            print a, b
-                            print c, d
-                            print
-                        
-    print("prime = %d, cnt = %d" % (p, cnt))
-    print("my formula = %d" % (p**4 - p**3 - p**2 + p))
+    print("prime = %d", p)
+    for e in range(p):
+        for f in range(p):
+            ls = []
+            for a in range(p):
+                for b in range(p):
+                    for c in range(p):
+                        for d in range(p):
+                            if not (a * d - b * c) % p == 0:
+                                ls += [((a * e + b * f) % p, (c * e + d * f) % p)]
+#                                print ("(%d, %d)" % ((a * e + b * f) % p, (c * e + d * f) % p))
+            print Set(sorted(ls))
