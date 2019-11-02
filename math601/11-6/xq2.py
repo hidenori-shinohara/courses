17576de8cb74992bdd9becf0079bd72805f85519
Hide Shinohara <hidenori.shinohara.jp@gmail.com>
2019-11-02 11:26:27 -0400

    Add xp2.py

--- math601/11-6/xq2.py
+++ math601/11-6/xq2.py
@@ -2,21 +2,19 @@ from sympy import *
 
 x = symbols('x')
 
-f = 1
-
-def irreducible(b, c, p):
+def irreducible2(b, c, p):
     for x in range(p):
         if (x * x + b * x + c) % p == 0:
             return False
     return True
 
-p = 2
+ps = [2, 3, 5, 7]
 
-
-
-for b in range(p):
-    for c in range(p):
-        if irreducible(b, c, p):
-            f = f * (x * x + b * x + c)
-print(f)
-print(expand(f))
+for p in ps:
+    f = 1
+    for b in range(p):
+        f = f * (x - b)
+        for c in range(p):
+            if irreducible2(b, c, p):
+                f = f * (x * x + b * x + c)
+    print(trunc(f, p))
