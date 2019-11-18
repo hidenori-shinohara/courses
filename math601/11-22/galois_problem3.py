10a267b6fe35e7d99ec32dd2913d6f1587d685c6
Hide Shinohara <hidenori.shinohara.jp@gmail.com>
2019-11-17 19:25:42 -0500

    Finish Problem 3(v)

--- math601/11-22/galois_problem3.py
+++ math601/11-22/galois_problem3.py
@@ -1,3 +1,4 @@
+from sympy.polys.polyfuncs import symmetrize
 from sympy import *
 from sympy.polys.orderings import monomial_key
 
@@ -11,5 +12,9 @@ v = x * y * y + y * z * z + z * x * x
 vv = y * x * x + x * z * z + z * y * y
 
 
-print(expand(v + vv - s1 * s2))
+#print("Problem 3(iii)")
+#print(expand(v + vv - s1 * s2))
 
+print("Problem 3(v)")
+print(symmetrize(v * vv))
+print(expand(symmetrize(v * vv)[0] - (9*s3**2 + s3*s1**3 - 6*s3*s1*s2 + s2**3)))
