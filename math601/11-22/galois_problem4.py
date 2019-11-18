5bf70534b10e8fccb80be5257fe1114968e094e1
Hide Shinohara <hidenori.shinohara.jp@gmail.com>
2019-11-18 05:27:40 -0500

    Update the code

--- math601/11-22/galois_problem4.py
+++ math601/11-22/galois_problem4.py
@@ -1,13 +1,13 @@
 from sympy.polys.polyfuncs import symmetrize
 from sympy import *
 
-u1, u2, u3 = symbols('u_1 u_2 u_3')
+u1, u2, u3 = symbols('u1 u2 u3')
 
 u = [u1, u2, u3]
 
 discriminant = 1
-for j in range(len(u)):
-    for i in range(j):
+for i in range(3):
+    for j in range(i + 1, 3):
         discriminant *= (u[i] - u[j]) * (u[i] - u[j])
 
 print(latex(symmetrize(discriminant, formal = True)[0]))
